d3dd545c057376595e1476a7947ceecb8eae0c23
yishuangxi <2546618112@qq.com>
2016-06-25 07:55:33 +0800

    订单金额

--- sql/carseller.sql
+++ sql/carseller.sql
@@ -1,7 +1,5 @@
 DROP database if EXISTS carseller;
-
-CREATE DATABASE IF NOT EXISTS carseller DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-
+CREATE DATABASE IF NOT EXISTS carseller DEFAULT CHARACTER SET UTF8 COLLATE UTF8_general_ci;
 use carseller;
 
 -- 用戶表
@@ -15,7 +13,7 @@ CREATE TABLE `user` (
   `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY  (`id`)
-) ENGINE=innodb  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=innodb  DEFAULT CHARSET=UTF8 AUTO_INCREMENT=1 ;
 
 -- 客戶表
 CREATE TABLE `client` (
@@ -26,7 +24,7 @@ CREATE TABLE `client` (
    `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
    `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
    PRIMARY KEY (`id`)
-  )ENGINE=innodb  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+  )ENGINE=innodb  DEFAULT CHARSET=UTF8 AUTO_INCREMENT=1;
 
 -- 用户-客户关系表：一个客户对应一个用户，一个用户对应多个客户
 CREATE TABLE `r_user_client`(
@@ -36,44 +34,31 @@ CREATE TABLE `r_user_client`(
   PRIMARY KEY (`id`),
   FOREIGN KEY f_key_1 (`user_id`) REFERENCES `user`(`id`),
   FOREIGN KEY f_key_2 (`client_id`) REFERENCES `client`(`id`)
-) ENGINE=innodb  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+) ENGINE=innodb  DEFAULT CHARSET=UTF8 AUTO_INCREMENT=1;
 
 -- 汽车表
-CREATE TABLE IF NOT EXISTS `car` (
-  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE TABLE `car` (
+  `id` INT(10) NOT NULL AUTO_INCREMENT,
   `vin` VARCHAR(20) NOT NULL UNIQUE COMMENT '车架号，唯一标记',
   `color` VARCHAR(20) NOT NULL DEFAULT '',
   `model` VARCHAR(20) NOT NULL COMMENT '车型号',
-  `status` ENUM('已售', '未售', '在途') NOT NULL,
+  `status` ENUM('已售', '未售', '在途') NOT NULL DEFAULT '在途',
+  `price` FLOAT(10, 2) NOT NULL DEFAULT 0 COMMENT '汽车价格',
   `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=innodb DEFAULT CHARACTER SET=utf8;
+) ENGINE=innodb DEFAULT CHARACTER SET=UTF8 AUTO_INCREMENT=1 ;
 
 -- -- 订单表
--- CREATE TABLE IF NOT EXISTS `order` (
---   `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
---   `car_id`  INT(10) NOT NULL COMMENT '汽车id',
---   `client_id` INT(10) NOT NULL UNIQUE COMMENT '客户id',
---   `status` ENUM('未支付', '已支付', '已取消') NOT NULL,
---   `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
---   `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
---   PRIMARY KEY (`id`),
---   FOREIGN KEY f_key_1 (`car_id`) REFERENCES `car`(`id`),
---   FOREIGN KEY f_key_2 (`client_id`) REFERENCES `client`(`id`)
--- ) ENGINE=innodb DEFAULT CHARACTER SET=utf8;
-
-
-
-
-
-
-
-
-
-
-
-
+CREATE TABLE `order`(
+  `id` INT(10) NOT NULL AUTO_INCREMENT COMMENT '关系表id',
+  `payment` FLOAT(10, 2) NOT NULL DEFAULT 0 COMMENT '实付款',
+  `car_id` INT(10) NOT NULL COMMENT '用户id无需唯一',
+  `client_id` INT(10) NOT NULL UNIQUE COMMENT '客户id要唯一',
+  PRIMARY KEY (`id`),
+  FOREIGN KEY f_key_1 (`car_id`) REFERENCES `car`(`id`),
+  FOREIGN KEY f_key_2 (`client_id`) REFERENCES `client`(`id`)
+) ENGINE=innodb DEFAULT CHARSET=UTF8 AUTO_INCREMENT=1;
 
 
 
