c31f6dc6d5b14ea05ce1aa2b93e7398ec8743aeb
yishuangxi <2546618112@qq.com>
2016-06-24 13:57:12 +0800

    car table

--- sql/carseller.sql
+++ sql/carseller.sql
@@ -6,45 +6,46 @@ use carseller;
 
 -- 用戶表
 CREATE TABLE `user` (
-  `id` INT(11) NOT NULL auto_increment COMMENT '用户ID',
+  `id` INT(10) NOT NULL AUTO_INCREMENT COMMENT '用户ID',
   `username` VARCHAR(20) NOT NULL DEFAULT '' COMMENT '用户名',
   `password` VARCHAR(20) NOT NULL,
   `phone` CHAR(11) DEFAULT NULL ,
-  `sex` enum('男','女') DEFAULT NULL ,
+  `sex` ENUM('男','女') DEFAULT NULL ,
   `status` INT(1) NOT NULL DEFAULT '0' COMMENT '1激活，0为禁用',
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp comment '创建时间',
-  `updated_at` timestamp NOT NULL DEFAULT current_timestamp comment '创建时间',
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   PRIMARY KEY  (`id`)
 ) ENGINE=innodb  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- 客戶表
 CREATE TABLE `client` (
-   `id` INT(11) NOT NULL auto_increment comment '客户ID',
-   `clientname` VARCHAR(50) NOT NULL DEFAULT '' comment '客户名',
-   `phone` INT(11) NOT NULL DEFAULT '0' comment '客户手机号码',
-   `sex` enum('男','女') DEFAULT NULL ,
-   `created_at` timestamp NOT NULL DEFAULT current_timestamp comment '创建时间',
-   `updated_at` timestamp NOT NULL DEFAULT current_timestamp comment '创建时间',
+   `id` INT(10) NOT NULL AUTO_INCREMENT COMMENT '客户ID',
+   `clientname` VARCHAR(50) NOT NULL DEFAULT '' COMMENT '客户名',
+   `phone` INT(10) NOT NULL DEFAULT '0' COMMENT '客户手机号码',
+   `sex` ENUM('男','女') DEFAULT NULL ,
+   `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+   `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
    PRIMARY KEY (`id`)
   )ENGINE=innodb  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
 
 -- 用户-客户关系表：一个客户对应一个用户，一个用户对应多个客户
-CREATE TABLE r_user_client(
-  `id` INT(11) NOT NULL auto_increment comment '关系表id',
-  `user_id` INT(11) NOT NULL ,
-  `client_id` INT(11) NOT NULL UNIQUE ,
+CREATE TABLE `r_user_client`(
+  `id` INT(10) NOT NULL AUTO_INCREMENT COMMENT '关系表id',
+  `user_id` INT(10) NOT NULL ,
+  `client_id` INT(10) NOT NULL UNIQUE ,
   PRIMARY KEY (`id`),
-  FOREIGN KEY f_key_1 (user_id) REFERENCES user(id),
-  FOREIGN KEY f_key_2 (client_id) REFERENCES client(id)
+  FOREIGN KEY f_key_1 (`user_id`) REFERENCES `user`(`id`),
+  FOREIGN KEY f_key_2 (`client_id`) REFERENCES `client`(`id`)
 ) ENGINE=innodb  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
 
 -- 汽车表
-CREATE TABLE IF NOT EXISTS car (
-  `id` INT(10) unsigned not null auto_increment PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS `car` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `vin` VARCHAR(20) NOT NULL UNIQUE COMMENT '车架号，唯一标记',
-  `name` VARCHAR(20) DEFAULT NULL COMMENT '车名字',
-  `color` INT(1) COMMENT '0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10',
+  `color` VARCHAR(20) NOT NULL DEFAULT '',
   `model` VARCHAR(20) NOT NULL COMMENT '车型号',
-  `created_at` DATETIME DEFAULT NOW(),
-  `updated_at` DATETIME DEFAULT NOW()
+  `status` ENUM('已售', '未售', '在途') ,
+  `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
 ) ENGINE=innodb DEFAULT CHARACTER SET=utf8;
